b9bef8876063ece7bc91490b05711d051b038fab
Guillaume Scigala <g.scigala@startmail.com>
2018-10-13 11:21:00 +0200

    WIP: Add a simple server

--- src/tcp-server.py
+++ src/tcp-server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+"""
 import socket
 import dbus
 
@@ -11,7 +11,7 @@ commands = {
     'on'   : "1",
     'off'  : "2",
     'state': "3"
-    }
+}
 
 sysbus = dbus.SystemBus()
 systemd1 = sysbus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
@@ -45,8 +45,8 @@ while 1:
             result = commands['off']
 
             activeState = proxy.Get('org.freedesktop.systemd1.Unit',
-                'ActiveState',
-                dbus_interface='org.freedesktop.DBus.Properties')
+                                    'ActiveState',
+                                    dbus_interface='org.freedesktop.DBus.Properties')
 
             if(activeState == 'active'):
                 result = commands['on']
@@ -55,3 +55,46 @@ while 1:
             
     conn.close()
     print "Connection closed"
+"""
+
+import argparse
+import socket
+import threading
+
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def listen(self):
+        self.sock.listen(1)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                if data:
+                    # Set the response to echo back the recieved data 
+                    response = data
+                    client.send(response)
+                else:
+                    raise error('Client disconnected')
+            except:
+                client.close()
+                return False
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("port", type=int,
+                        help="Port to listen")
+    args = parser.parse_args()
+    
+    ThreadedServer('',args.port).listen()
