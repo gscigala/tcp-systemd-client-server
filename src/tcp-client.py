66a0aad0adb3b00413f3ed1b4ad89d53f583640c
Guillaume Scigala <guillaume.scigala@smile.fr>
2018-10-17 18:35:59 +0200

    WIP: began implementation of the client, TO TEST

--- src/tcp-client.py
+++ src/tcp-client.py
@@ -1,10 +1,69 @@
 #!/usr/bin/env python
 
+
+import logging
+import logging.config
+import argparse
 import sys
 import socket
 
 import sys, getopt
 
+from message import FrameData
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ip", type=str,
+                        help="Destination address")
+    parser.add_argument("port", type=int,
+                        help="Port to listen")
+    parser.add_argument("--getState", type=int,
+                        help="Get state of the device")
+    parser.add_argument("--state", type=int,
+                        help="Set state of the device")
+    parser.add_argument("--dimmer", type=int,
+                        help="Set dimmer of the device")
+    args = parser.parse_args()
+
+    logging.config.fileConfig("logging.conf")
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((args.ip, args.port))
+
+    # Initialize message
+    obj = FrameData.parse(data[:FrameData.sizeof()])
+    obj.GetState = False
+    obj.State = False
+    obj.Dimmer = 255
+
+    if(args.getState == True):
+        self.logger.info("Get State")
+        obj.GetState = True
+        s.send(obj)
+        data = s.recv(BUFFER_SIZE)
+
+        #TODO process received frame
+
+    else:
+        # Process state
+        if(args.state == True): #TODO test if exist too !
+            self.logger.info("Send state ON")
+            obj.State = True
+        else:
+            self.logger.info("Send state OFF")
+
+        # Process dimmer
+        if (args.dimmer != 255):
+            self.logger.info("Send dimmer")
+            obj.Dimmer = args.dimmer
+        else:
+            self.logger.info("No dimmer")
+
+        s.send(obj)
+        
+        
+
+"""
 TCP_IP = '192.168.22.101'
 TCP_PORT = 15333
 BUFFER_SIZE = 1024
@@ -55,3 +114,4 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+"""
